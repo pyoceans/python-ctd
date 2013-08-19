c5fb716c218756ddbc990d02feb1252d72a8d1b0
ocefpaf <ocefpaf@gmail.com>
2013-08-19 07:24:20 -0300

    Simplified check plot functions and section.

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -7,7 +7,7 @@
 # e-mail:   ocefpaf@gmail
 # web:      http://ocefpaf.tiddlyspot.com/
 # created:  01-Mar-2013
-# modified: Sun 18 Aug 2013 08:13:00 PM BRT
+# modified: Mon 19 Aug 2013 06:49:50 AM BRT
 #
 # obs:
 #
@@ -24,7 +24,7 @@ from pandas.util.testing import ensure_clean
 from pandas import Panel
 import matplotlib.pyplot as plt
 
-from ctd import DataFrame, Series, movingaverage, lp_filter, derive_cnv
+from ctd import DataFrame, Series
 
 
 data_path = os.path.join(os.path.dirname(__file__), 'data')
@@ -45,48 +45,20 @@ def assert_is_valid_plot_return_object(objs):
 
 
 def _check_plot_works(f, *args, **kwargs):
-    try:
-        fig = kwargs['figure']
-    except KeyError:
-        fig = plt.gcf()
-    plt.clf()
-    ax = kwargs.get('ax', fig.add_subplot(211))
-    ret = f(*args, **kwargs)
+    fig, ax = f(*args, **kwargs)
 
-    assert ret is not None
-    assert_is_valid_plot_return_object(ret)
-
-    try:
-        kwargs['ax'] = fig.add_subplot(212)
-        ret = f(*args, **kwargs)
-    except Exception:
-        pass
-    else:
-        assert_is_valid_plot_return_object(ret)
+    assert fig is not None
+    assert_is_valid_plot_return_object(ax)
 
     with ensure_clean() as path:
         plt.savefig(path)
 
 
 def _check_section_works(f, **kwargs):
-    try:
-        fig = kwargs['figure']
-    except KeyError:
-        fig = plt.gcf()
-    plt.clf()
-    ax = kwargs.get('ax', fig.add_subplot(211))
-    ret = f(**kwargs)
+    fig, ax, cb = f(**kwargs)
 
-    assert ret is not None
-    assert_is_valid_plot_return_object(ret)
-
-    try:
-        kwargs['ax'] = fig.add_subplot(212)
-        ret = f(**kwargs)
-    except Exception:
-        pass
-    else:
-        assert_is_valid_plot_return_object(ret)
+    assert fig is not None
+    assert_is_valid_plot_return_object(ax)
 
     with ensure_clean() as path:
         plt.savefig(path)
@@ -98,69 +70,40 @@ def alphanum_key(s):
     return key
 
 
-def proc_ctd(fname, compression='gzip', below_water=True):
+def proc_ctd(fname):
     """Quick-n-dirty CTD processing."""
-    # 00-Split, clean 'bad pump' data, and apply flag.
-    cast = DataFrame.from_cnv(fname, compression=compression,
-                              below_water=below_water).split()[0]
+    cast = DataFrame.from_cnv(fname, compression='gzip',
+                              below_water=True).split()[0]
     cast = cast[cast['pumps']]
-    cast = cast[~cast['flag']]  # True for bad values.
+    cast = cast[~cast['flag']]
     name = os.path.basename(fname).split('.')[0]
-
     # Removed unwanted columns.
-    keep = set(['altM', 'c0S/m', 'dz/dtM', 'wetCDOM', 'latitude',
-                'longitude', 'sbeox0Mm/Kg', 'sbeox1Mm/Kg', 'oxsolMm/Kg',
-                'oxsatMm/Kg', 'par', 'pla', 'sva', 't090C', 't190C', 'tsa',
-                'sbeox0V'])
-
+    keep = set(['t090C', 't190C', 'longitude', 'latitude'])
     null = map(cast.pop, keep.symmetric_difference(cast.columns))
     del null
-
-    # Smooth velocity with a 2 seconds windows.
-    cast['dz/dtM'] = movingaverage(cast['dz/dtM'], window_size=48)
-
-    # 01-Filter pressure.
-    kw = dict(sample_rate=24.0, time_constant=0.15)
-    cast.index = lp_filter(cast.index, **kw)
-
-    # 02-Remove pressure reversals.
-    cast = cast.press_check()
-    cast = cast.dropna()
-
-    # 03-Loop Edit.
-    cast = cast[cast['dz/dtM'] >= 0.25]  # Threshold velocity.
-
-    # 04-Remove spikes.
-    kw = dict(n1=2, n2=20, block=100)
-    cast = cast.apply(Series.despike, **kw)
-
-    # 05-Bin-average.
     cast = cast.apply(Series.bindata, **dict(delta=1.))
-
-    # 06-interpolate.
     cast = cast.apply(Series.interpolate)
-
-    if False:
-        # 07-Smooth.
-        pmax = max(cast.index)
-        if pmax >= 500.:
-            window_len = 21
-        elif pmax >= 100.:
-            window_len = 11
-        else:
-            window_len = 5
-        kw = dict(window_len=window_len, window='hanning')
-        cast = cast.apply(Series.smooth, **kw)
-
-    # 08-Derive.
-    cast.lat = cast['latitude'].mean()
-    cast.lon = cast['longitude'].mean()
-    try:
-        cast = derive_cnv(cast)
-    except:
-        raise nose.SkipTest
     cast.name = name
     return cast
+
+
+def make_section(data_path=data_path, variable='t090C'):
+    lon, lat = [], []
+    section = OrderedDict()
+    pattern = '%s/CTD/g01mcan*c.cnv.gz' % data_path
+    fnames = sorted(glob(pattern), key=alphanum_key)
+    for fname in fnames:
+        cast = proc_ctd(fname)
+        name = os.path.basename(fname).split('.')[0]
+        section.update({name: cast})
+        lon.append(cast.longitude.mean())
+        lat.append(cast.latitude.mean())
+
+    section = Panel.fromDict(section)
+    section = section.minor_xs(variable)
+    # Section (FIXME: Propagate lon, lat with MetaDataFrame).
+    section.lon, section.lat = lon, lat
+    return section
 
 
 class PlotUtilities(unittest.TestCase):
@@ -210,37 +153,24 @@ class AdvancedPlotting(unittest.TestCase):
             raise nose.SkipTest
 
     def setUp(self):
-        lon, lat = [], []
-        pattern = '%s/CTD/g01mcan*c.cnv.gz' % data_path
-        fnames = sorted(glob(pattern), key=alphanum_key)
-        section = OrderedDict()
-        for fname in fnames:
-            cast = proc_ctd(fname)
-            name = os.path.basename(fname).split('.')[0]
-            section.update({name: cast})
-            lon.append(cast.longitude.mean())
-            lat.append(cast.latitude.mean())
-
-        # Section (FIXME: Propagate lon, lat with MetaDataFrame).
-        self.section = Panel.fromDict(section)
-        self.CT = self.section.minor_xs('CT')
-        self.CT.lon, self.CT.lat = lon, lat
+        self.t090C = make_section(data_path=data_path, variable='t090C')
 
     def tearDown(self):
         import matplotlib.pyplot as plt
         plt.close('all')
 
     def test_section_reverse(self):
-        _check_section_works(self.CT.plot_section, reverse=True)
+        _check_section_works(self.t090C.plot_section, reverse=True)
 
     def test_section_reverse_filled(self):
-        _check_section_works(self.CT.plot_section, reverse=True, filled=True)
+        _check_section_works(self.t090C.plot_section, reverse=True,
+                             filled=True)
 
     def test_section(self):
-        _check_section_works(self.CT.plot_section)
+        _check_section_works(self.t090C.plot_section)
 
     def test_section_filled(self):
-        _check_section_works(self.CT.plot_section, filled=True)
+        _check_section_works(self.t090C.plot_section, filled=True)
 
 
 if __name__ == '__main__':

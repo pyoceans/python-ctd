000eff4cc5bb60bc46a873def731a90c28cfb9ca
ocefpaf <ocefpaf@gmail.com>
2013-08-19 07:25:01 -0300

    Added a full section processing step.

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -7,15 +7,93 @@
 # e-mail:   ocefpaf@gmail
 # web:      http://ocefpaf.tiddlyspot.com/
 # created:  01-Mar-2013
-# modified: Wed 24 Jul 2013 08:17:36 PM BRT
+# modified: Mon 19 Aug 2013 06:45:09 AM BRT
 #
 # obs:
 #
 
-
+import os
+import re
+import nose
 import unittest
+from glob import glob
+from collections import OrderedDict
+
 import numpy as np
-from ctd import DataFrame, lp_filter, derive_cnv
+from ctd import DataFrame, Series, lp_filter, derive_cnv, movingaverage
+
+data_path = os.path.join(os.path.dirname(__file__), 'data')
+
+
+def alphanum_key(s):
+    key = re.split(r"(\d+)", s)
+    key[1::2] = map(int, key[1::2])
+    return key
+
+
+def proc_ctd(fname, compression='gzip', below_water=True):
+    """Quick-n-dirty CTD processing."""
+    # 00-Split, clean 'bad pump' data, and apply flag.
+    cast = DataFrame.from_cnv(fname, compression=compression,
+                              below_water=below_water).split()[0]
+    cast = cast[cast['pumps']]
+    cast = cast[~cast['flag']]  # True for bad values.
+    name = os.path.basename(fname).split('.')[0]
+
+    # Removed unwanted columns.
+    keep = set(['altM', 'c0S/m', 'dz/dtM', 'wetCDOM', 'latitude',
+                'longitude', 'sbeox0Mm/Kg', 'sbeox1Mm/Kg', 'oxsolMm/Kg',
+                'oxsatMm/Kg', 'par', 'pla', 'sva', 't090C', 't190C', 'tsa',
+                'sbeox0V'])
+
+    null = map(cast.pop, keep.symmetric_difference(cast.columns))
+    del null
+
+    # Smooth velocity with a 2 seconds windows.
+    cast['dz/dtM'] = movingaverage(cast['dz/dtM'], window_size=48)
+
+    # 01-Filter pressure.
+    kw = dict(sample_rate=24.0, time_constant=0.15)
+    cast.index = lp_filter(cast.index, **kw)
+
+    # 02-Remove pressure reversals.
+    cast = cast.press_check()
+    cast = cast.dropna()
+
+    # 03-Loop Edit.
+    cast = cast[cast['dz/dtM'] >= 0.25]  # Threshold velocity.
+
+    # 04-Remove spikes.
+    kw = dict(n1=2, n2=20, block=100)
+    cast = cast.apply(Series.despike, **kw)
+
+    # 05-Bin-average.
+    cast = cast.apply(Series.bindata, **dict(delta=1.))
+
+    # 06-interpolate.
+    cast = cast.apply(Series.interpolate)
+
+    if False:
+        # 07-Smooth.
+        pmax = max(cast.index)
+        if pmax >= 500.:
+            window_len = 21
+        elif pmax >= 100.:
+            window_len = 11
+        else:
+            window_len = 5
+        kw = dict(window_len=window_len, window='hanning')
+        cast = cast.apply(Series.smooth, **kw)
+
+    # 08-Derive.
+    cast.lat = cast['latitude'].mean()
+    cast.lon = cast['longitude'].mean()
+    try:
+        cast = derive_cnv(cast)
+    except:
+        raise nose.SkipTest
+    cast.name = name
+    return cast
 
 
 class BasicProcessingTests(unittest.TestCase):
@@ -85,9 +163,21 @@ class BasicProcessingTests(unittest.TestCase):
                         sorted(new_cols))
 
 
-def main():
-    unittest.main()
+class AdvancedProcessingTests(unittest.TestCase):
+
+    def setUp(self):
+        lon, lat = [], []
+        pattern = '%s/CTD/g01mcan*c.cnv.gz' % data_path
+        fnames = sorted(glob(pattern), key=alphanum_key)
+        section = OrderedDict()
+        for fname in fnames:
+            cast = proc_ctd(fname)
+            name = os.path.basename(fname).split('.')[0]
+            section.update({name: cast})
+            lon.append(cast.longitude.mean())
+            lat.append(cast.latitude.mean())
+    # TODO: Write section tests.
 
 
 if __name__ == '__main__':
-    main()
+    unittest.main()

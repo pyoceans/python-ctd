c2a37e329ee9183dd63c72822de5eaa2ff130a57
ocefpaf <ocefpaf@gmail.com>
2013-08-31 10:33:38 -0300

    Removed octave from travis.

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -5,7 +5,7 @@
 SITE_PKG_DIR=$VIRTUAL_ENV/lib/python$TRAVIS_PYTHON_VERSION/site-packages
 echo "Using SITE_PKG_DIR: $SITE_PKG_DIR"
 
-# Workaround for travis ignoring system_site_packages in travis.yml
+# Workaround for travis ignoring system_site_packages in travis.yml.
 rm -f $VIRTUAL_ENV/lib/python$TRAVIS_PYTHON_VERSION/no-global-site-packages.txt
 
 sudo apt-get update -qq
@@ -13,6 +13,3 @@ sudo apt-get install -qq build-essential
 sudo apt-get install -qq build-dep python-scipy
 sudo apt-get install -qq build-dep python-numpy
 sudo apt-get install -qq build-dep python-pandas
-
-sudo apt-get install -qq octave
-sudo apt-get install -qq octave-pkg-dev

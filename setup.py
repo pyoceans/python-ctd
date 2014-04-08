3e5eb415efa696348907a23c82039c3213ed5992
ocefpaf <ocefpaf@gmail.com>
2014-04-08 10:08:37 -0300

    Wrapped read for long_description.

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import io
+import re
 from setuptools import setup
 
-import re
 VERSIONFILE="ctd/__init__.py"
 verstrline = open(VERSIONFILE, "rt").read()
 VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
@@ -12,6 +13,19 @@ if mo:
     verstr = mo.group(1)
 else:
     raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read('README.txt', 'CHANGES.txt')
+LICENSE = open('LICENSE.txt').read()
 
 
 source = 'http://pypi.python.org/packages/source'
@@ -31,17 +45,13 @@ Topic :: Education
 Topic :: Software Development :: Libraries :: Python Modules
 """
 
-README = open('README.md').read()
-CHANGES = open('CHANGES.txt').read()
-LICENSE = open('LICENSE.txt').read()
-
 config = dict(name='ctd',
               version=verstr,
               packages=['ctd'],
               test_suite='tests',
               use_2to3=True,
               license=LICENSE,
-              long_description='%s\n\n%s'.encode('utf-8') % (README, CHANGES),
+              long_description=long_description,
               classifiers=filter(None, classifiers.split("\n")),
               description='Tools to load hydrographic data as DataFrames',
               author='Filipe Fernandes',

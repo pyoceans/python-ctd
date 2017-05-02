7e6ed1aa76f792b11c5f5671d9b58ca42993f9dd
ocefpaf <ocefpaf@gmail.com>
2017-05-02 10:47:41 -0300

    proper future imports

--- ctd/plotting.py
+++ ctd/plotting.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import absolute_import, unicode_literals
+from __future__ import (absolute_import, division, print_function)
 
 # Scientific stack.
 import numpy as np

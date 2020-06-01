afa8951aedcd854ff34a20ea89190416cff6d60a
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-06-01 15:32:38 +0000

    Update docs after building Travis build 315 of pyoceans/python-ctd
    The docs were built from the branch 'master' against the commit
    fba388e730f275a9b07f7968fbecfe01de6eb4da.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/pyoceans/python-ctd/jobs/693498906.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/TEST/lib/python3.8/site-packages/doctr/__main__.py deploy --build-tags --key-path github_deploy_key_pyoceans_python_ctd.enc --built-docs docs/build/html/ dev

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.1.dev28+g423e789',
+    VERSION: '1.0.1.dev30+gfba388e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

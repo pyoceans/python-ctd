5103b4b86f719111e1a1dc8726d4b54a7c9ecd50
Filipe Fernandes <ocefpaf@gmail.com>
2021-11-16 17:09:05 -0300

    fix github dependency graph issue

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 
 setup(
+    # The package metadata is specified in setup.cfg but GitHub's downstream dependency graph
+    # does not work unless we put the name this here too.
+    name="ctd",
     use_scm_version={
         "write_to": "ctd/_version.py",
         "write_to_template": '__version__ = "{version}"',

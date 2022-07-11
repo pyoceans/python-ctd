c52466f99f91935c14df78548e1b38102878e60a
ocefpaf <ocefpaf@users.noreply.github.com>
2022-07-11 19:30:21 +0000

    deploy: 30c5bcb80dc76aa8391fc7482455d6872c35d27c

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.4.2.dev4+gafddde1',
+    VERSION: '1.4.2.dev6+g30c5bcb',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
